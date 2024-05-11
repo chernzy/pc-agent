50f8d692c4291e00c8b067fab8e4a4dbb686ae95
Lucky <lucky_soft@163.com>
2024-05-11 15:54:17 +0800

    feat: add web page

---
+++ web/custom_typings/can-ndjson-stream/index.d.ts
@@ -0,0 +1,12 @@
+declare module "can-ndjson-stream" {
+    export default function ndjsonStream(data: unknown): {
+      getReader: () => {
+        read: () => Promise<{
+          done: boolean
+          value: any
+        }>
+      }
+      cancel: () => void
+    }
+  }
+  
